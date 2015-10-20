7d3a8be884c32507ffef1c8926541dba3e008026
Esirnus <cxf@chenxiaofeidemacbook-pro.local>
2015-10-20 21:53:40 +0800

    更新README.MD

--- ESFrameWork/ESFrameWork/Classes/Other/Category/UIBarButtonItem+EGExtention.m
+++ ESFrameWork/ESFrameWork/Classes/Other/Category/UIBarButtonItem+EGExtention.m
@@ -10,10 +10,9 @@
 @implementation UIBarButtonItem (ESExtension)
 /**
  *  抽取此处代码有三种方式
- *  1.继承:不采用
- *  2.新建一个类:不采用
- *  3.分类:符合苹果的内部的做法,清晰明确
- *
+ *  继承:不采用
+ *  新建一个类:不采用
+ *  分类:符合苹果的内部的做法,清晰明确
  */
 + (instancetype)itemWithImage:(NSString *)image highImage:(NSString *)highImage target:(id)target action:(SEL)action {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
